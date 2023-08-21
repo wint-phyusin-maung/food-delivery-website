d2ad284ec1d0d9389eefc633ed6ba516f8091798
Shonn-Shonn <sshonn687@gmail.com>
2023-08-21 16:55:11 +0630

    add: new features

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,10 @@
+<template>
+   <div class="something">
+       hello Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid, voluptate officia. Ullam, rerum. Suscipit sit, in, repellat hic quibusdam libero dolorem, similique impedit officiis tempore obcaecati voluptas molestiae et natus?
+   </div>
+</template>
+
+
+<script>
+  export default{}
+</script>
