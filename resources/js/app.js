11b2b212bb7801fd491e5275e243594fd6a898e2
Shonn-Shonn <sshonn687@gmail.com>
2023-08-23 19:25:37 +0630

    add: spinner and progress bar in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,9 +6,15 @@ createInertiaApp({
     const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
     return pages[`./Pages/${name}.vue`]
   },
+  progress: {
+    color: '#f88379',
+    delay: 200,
+    showSpinner: true,
+  },
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
       .mount(el)
   },
 })
+
