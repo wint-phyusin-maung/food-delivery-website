d91c0800438e7166cddfcd3d4ff74cd355c288df
Shonn-Shonn <sshonn687@gmail.com>
2023-08-24 14:53:52 +0630

    add: navlink component

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -1,20 +1,30 @@
 <template>
     <nav class="mt-6">
         <ul class="list-none">
-            <li><Link href="/" class="text-blue-500 hover:underline">Home</Link></li>
-            <li><Link href="/users" class="text-blue-500 hover:underline">Users</Link></li>
-            <li><Link href="/settings" class="text-blue-500 hover:underline">Settings</Link></li>
-            <li><Link href="/logout" method="post" :data="{foo: 'bar'}" as="button" class="text-blue-500 hover:underline">Logout</Link></li>
+          
+            <li>
+                <NavLink href="/" :active="$page.component === 'Home'">Home</NavLink>
+            </li>
+            <li>
+               <NavLink href="/users" :active="$page.component === 'Users'">Users</NavLink>
+            </li>
+            <li>
+               <NavLink href="/settings" :active="$page.component === 'Settings'">Settings</NavLink>
+            </li>
+            <li>
+                <Link href="/logout" method="post" :data="{foo: 'bar'}" as="button" class="text-blue-500 hover:underline">Logout</Link>
+            </li>
         </ul>
     </nav>
 </template>
 
 <script>
     import { Link } from '@inertiajs/vue3'
+    import NavLink from '../Shared/NavLink.vue'
 
     export default {
     components: {
-        Link,
+        Link,NavLink
     },
 };
 </script>
