e27b6f82e51b76ad25e2f975765d255149624aed
Shonn-Shonn <sshonn687@gmail.com>
2023-09-03 19:47:16 +0630

    use:ref

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,6 +1,10 @@
 <template>
         <h1 class="text-4xl">Home</h1>
+        <button @click="count++">Add 1</button>
+        <p>Count is: {{ count }}</p>
 </template>
 
-<script>
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
 </script>
